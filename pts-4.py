3d613ed0fbf389586084ab840acd1dd5396d7c0e
Joao MC Teixeira <joaomcteixeira@gmail.com>
2019-09-05 10:26:48 -0400

    updated pts-4

--- pts-4.py
+++ pts-4.py
@@ -10,6 +10,15 @@
 # on doubts please search elsewhere on the web, there are countless of
 # amazing explanations; here, we focus on the broader concept
 
+
+# EDIT 2019/09/05
+# There's a very interesting fault in the design shown in the original example
+# of this post, I came to understand it when I was actually using the pattern.
+# Can you spot it before hand? Bellow is the original example from this post,
+# and, at the end, an updated explanation.
+
+
+# The original example:
 from abc import ABC, abstractmethod
 
 
@@ -58,6 +67,79 @@ class SubClass1(Interface):
 
 
 class SubClass2(Interface):
+    
+    def __init__(self, arg1):
+        # do init magic
+        
+    def run(self):
+        #run magic
+        
+        self._private_meth_1()
+        self._private_meth_2()
+        
+        return
+    
+    def _private_meth_1(self):
+        return    
+    
+    def _private_meth_2(self):
+        return
+    
+    def _private_meth_3(self):
+        return
+
+# An updated discussion:
+from abc import ABC, abstractmethod
+
+
+class Interface:
+    
+    def __new__(cls, arg1):
+        
+        match_d = {
+            # evalutes arguments based on conditions to decide which
+            # subclass to instantiate
+            is_for_subclass1(arg1): SubClass1,
+            is_for_subclass2(arg1): SubClass2,
+            # etc...
+            }
+    
+        try:
+            # this actually calls object.__new__(match_d[True])
+            # dont know if it is better to super(Interface) or object.
+            return super(Interface, cls).__new__(match_d[True])
+        except KeyError:
+            raise NotImplementedError from None
+
+
+class ClassBase(ABC):
+    
+    @abstractmethod
+    def run(self):
+        return
+
+
+class SubClass1(ClassBase):
+    
+    def __init__(self, arg1):
+        # do init magic
+        
+    def run(self):
+        #run magic
+        
+        self._private_meth_1()
+        self._private_meth_2()
+        
+        return
+    
+    def _private_meth_1(self):
+        return    
+    
+    def _private_meth_2(self):
+        return
+
+
+class SubClass2(ClassBase):
     
     def __init__(self, arg1):
         # do init magic
